e004546f73c10a68e59facff706cc52d8691a89a
UON51 <alex88080751@gmail.com>
2020-05-21 23:14:47 +0800

    login buttom finish

--- app/src/main/java/com/example/nm_store_system/MainActivity.java
+++ app/src/main/java/com/example/nm_store_system/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.nm_store_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public EditText editText_account;
+    public EditText editText_passwd;
+    String[] AP = new String[1];
+    Boolean result = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText_account = findViewById(R.id.editText_account);
+        editText_passwd = findViewById(R.id.editText_passwd);
+
+    }
+
+    public void buttom_loging(View view) {
+        AP[0] = String.valueOf(editText_account.getText());
+        AP[1] = String.valueOf(editText_passwd.getText());
+        Thread thread_login = new login();
+        thread_login.start();
+        try {
+            thread_login.join();
+            if (result = true){
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,store_main.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("account",AP[0]);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            } else {
+                Toast.makeText(MainActivity.this,"帳號或密碼錯誤，請重式", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e){
+
+        }
+    }
+    public class login extends Thread{
+        public void run(){
+            MysqlCon con = new MysqlCon();
+            con.connect_sql();
+            result = con.APD(AP[0],AP[1]);
+        }
     }
 }
---
+++ app/src/main/java/com/example/nm_store_system/MysqlCon.java
@@ -0,0 +1,61 @@
+package com.example.nm_store_system;
+
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MysqlCon {
+    String mysql_ip = "35.201.205.223";
+    int mysql_port = 3306; // Port 預設為 3306
+    String db_name = "Night_Market_management";
+    String url = "jdbc:mysql://" + mysql_ip + ":" + mysql_port + "/" + db_name;
+    String db_user = "app-management";
+    String db_password = "management";
+
+
+    public Boolean connect_sql() {
+        //載入驅動
+        Boolean connect = false;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Log.v("DB", "加載驅動成功");
+        } catch (ClassNotFoundException e) {
+            Log.e("DB", "加載驅動失敗");
+
+        }
+        Connection con = null;
+        // 連接資料庫
+        try {
+            con = DriverManager.getConnection(url, db_user, db_password);
+            Log.v("DB", "遠端連接成功");
+            connect = true;
+        } catch (SQLException e) {
+            Log.e("DB", "遠端連接失敗");
+            Log.e("DB", e.toString());
+        }
+        return connect;
+    }
+    public boolean APD(String A,String P){
+        boolean result = false;
+        try {
+            Connection con =  DriverManager.getConnection(url, db_user, db_password);
+            Statement st = con.createStatement();
+            ResultSet respond = st.executeQuery("select Account from Store_Account where Account='"+ A + "' and Password= '"+P+"'");
+            while (respond.next()){
+                String Z =respond.getString("Account");
+                if(Z.equals(A)){
+                    result = true;
+                } else {
+                    result = false;
+                }
+            }
+        } catch (Exception e){
+
+        }
+        return result;
+    }
+}

739342d1315b5b43ff005ef0afbdc705fd4f3936
UON <alex88080751@gmail.com>
2020-05-26 16:31:47 +0800

    New Activity 2020/5/26 UON

--- app/src/main/java/com/example/nm_store_system/MainActivity.java
+++ app/src/main/java/com/example/nm_store_system/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nm_store_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     public EditText editText_account;
     public EditText editText_passwd;
-    String[] AP = new String[1];
+    String[] AP = new String[2];
     Boolean result = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +27,34 @@ public class MainActivity extends AppCompatActivity {
     public void buttom_loging(View view) {
         AP[0] = String.valueOf(editText_account.getText());
         AP[1] = String.valueOf(editText_passwd.getText());
-        Thread thread_login = new login();
-        thread_login.start();
-        try {
-            thread_login.join();
-            if (result = true){
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this,store_main.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("account",AP[0]);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            } else {
-                Toast.makeText(MainActivity.this,"帳號或密碼錯誤，請重式", Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception e){
+        if (AP[1].equals("") || AP[0].equals(""))
+        {
+            editText_account.setHint("請輸入帳號");
+            editText_passwd.setHint("請輸入密碼");
+            editText_account.setHintTextColor(Color.RED);
+            editText_passwd.setHintTextColor(Color.RED);
 
+        } else {
+            Thread thread_login = new login();
+            thread_login.start();
+            try {
+                thread_login.join();
+
+                if (result = true){
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this,store_main.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("account",AP[0]);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this,"帳號或密碼錯誤，請重式", Toast.LENGTH_LONG).show();
+                }
+            } catch (Exception e){
+                Toast.makeText(MainActivity.this,"伺服器連接失敗,請確認網路狀態",Toast.LENGTH_LONG).show();
+            }
         }
+
     }
     public class login extends Thread{
         public void run(){
